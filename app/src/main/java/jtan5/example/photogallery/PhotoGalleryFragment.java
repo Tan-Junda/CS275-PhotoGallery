add4fd0f7585908ec9dd5af01c39b6dd39a858a2
Tan-Junda <jtan5@uvm.edu>
2019-10-10 07:53:37 +0800

    25.5

--- app/src/main/java/jtan5/example/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/jtan5/example/photogallery/PhotoGalleryFragment.java
@@ -1,27 +1,42 @@
 package jtan5.example.photogallery;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PhotoGalleryFragment extends Fragment {
     private RecyclerView mPhotoRecyclerView;
-
+    private static final String TAG = "PhotoGalleryFragment";
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
     }
-
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... parms) {
+            try {
+                String result = new FlickrFetchr().getUrlString("https://www.baidu.com");
+                Log.i(TAG, "Fetched");
+            } catch (IOException ioe) {
+                Log.e(TAG, "Fetched contents of URL: " + ioe);
+            }
+            return null;
+        }
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
