0cfee7a679cc1f9644f1d6c6325908321d0a6646
Tan-Junda <jtan5@uvm.edu>
2019-10-15 04:25:16 +0800

    26.5

--- app/src/main/java/jtan5/example/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/jtan5/example/photogallery/PhotoGalleryFragment.java
@@ -27,6 +27,7 @@ public class PhotoGalleryFragment extends Fragment {
     private RecyclerView mPhotoRecyclerView;
     private static final String TAG = "PhotoGalleryFragment";
     private List<GalleryItem> mItems = new ArrayList<>();
+    private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -49,6 +50,10 @@ public class PhotoGalleryFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
         new FetchItemsTask().execute();
+        mThumbnailDownloader = new ThumbnailDownloader();
+        mThumbnailDownloader.start();
+        mThumbnailDownloader.getLooper();
+        Log.i(TAG, "Background thread started");
     }
 
     @Override
@@ -103,6 +108,10 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
     }
-
-
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mThumbnailDownloader.quit();
+        Log.i(TAG, "Background thread destroyed");
+    }
 }
